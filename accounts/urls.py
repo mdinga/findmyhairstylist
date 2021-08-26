9faed28b5ebb65261ae0fb678492c0cbe5f81494
Mbasa <Mbasa>
2021-08-26 11:25:01 +0200

    Password reset system done

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('stylist_signup/', views.StylistSignUp.as_view(), name = 'stylist_signup'),
     path('client_signup/', views.ClientSignUp.as_view(), name = 'client_signup'),
     path('user_delete//<int:pk>/', views.UserDeleteView.as_view(), name = 'user_delete'),
-
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html", success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html", success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
